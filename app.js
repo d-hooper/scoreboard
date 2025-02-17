00575284d4d240d2d13ba2f5ace6c5f5a5568bcb
d-hooper <dallinhoop3@gmail.com>
2025-02-17 15:44:37 -0700

    Added JS functionality to home and away sides, as well as styling

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
 // SECTION  BRAIN
 
+let homeScore = 0
+
+let awayScore = 0
+
 // !SECTION
 
 // SECTION  LOGIC
 
+function updateHomeScore(amount) {
+  homeScore += amount
+  drawHomeScore()
+}
+
+function updateAwayScore(amount) {
+  awayScore += amount
+  drawAwayScore()
+}
+
 // !SECTION
 
 // SECTION  DRAW/UPDATE
+
+function drawHomeScore() {
+  const homeScoreElem = document.getElementById('homeScore')
+  homeScoreElem.innerText = homeScore
+}
+
+function drawAwayScore() {
+  const awayScoreElem = document.getElementById('awayScore')
+  awayScoreElem.innerText = awayScore
+}
+
+function resetScore() {
+  homeScore = 0
+  awayScore = 0
+  drawHomeScore()
+  drawAwayScore()
+}
 
 // !SECTION
